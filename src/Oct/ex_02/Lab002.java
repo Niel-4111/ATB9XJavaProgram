0a3a9aec192c4a576d866099504e0816ea953a7d
niles <nilesh1998sonawane@gmail.com>
2024-11-12 00:14:48 +0530

    all files as of 11/12

--- src/Oct/ex_02/Lab002.java
+++ src/Oct/ex_02/Lab002.java
@@ -2,6 +2,24 @@ package Oct.ex_02;
 
 public class Lab002 {
     public static void main(String[] args) {
-        System.out.println("Hey");
+        int age = 10;
+        System.out.printf("your age is %d",age);
+        System.out.println();
+        String name = "Nilesh";
+        System.out.printf("Your name is %s", name);
+        System.out.println();
+        System.out.println();
+        System.out.println("-----Table code using printf-----");
+        int table = 10;
+        System.out.printf("%d * %d = %d", table, 1, table*1);
+        System.out.printf("\n%d * %d = %d", table, 2, table*2);
+        System.out.printf("\n%d * %d = %d", table, 3, table*3);
+        System.out.printf("\n%d * %d = %d", table, 4, table*4);
+        System.out.printf("\n%d * %d = %d", table, 5, table*5);
+        System.out.printf("\n%d * %d = %d", table, 6, table*6);
+        System.out.printf("\n%d * %d = %d", table, 7, table*7);
+        System.out.printf("\n%d * %d = %d", table, 8, table*8);
+        System.out.printf("\n%d * %d = %d", table, 9, table*9);
+        System.out.printf("\n%d * %d = %d", table, 10, table*10);
     }
 }
